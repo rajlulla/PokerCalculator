e50450c98d8a45b81eb54edf96a79c5d7532bb28
rajlulla <raj@haylontech.com>
2024-05-21 12:06:02 -0700

    Initial Commit

---
+++ PokerCalculator/PokerCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokerCalculatorApp.swift
+//  PokerCalculator
+//
+//  Created by Raj Lulla on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PokerCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
